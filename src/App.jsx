73dc7ddbf2debf4a075db29cba67f5c0328309fc
wayne <waynekim07@gmail.com>
2025-06-02 14:40:48 +0300

    simple input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
-import './App.css'
+import List from './Components/List'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [inp,setInp] = useState([])
+  const getItems = e=>setInp(prevInp=>[...prevInp,...e.split(",").map((item,index)=><li key={index}>{item}</li>)])
   return (
     <>
+      
+      <input 
+        id='inp'
+        type="text"
+      />
+
+      <button
+        onClick={()=>getItems(document.querySelector("#inp").value)}
+      > Add </button>
+      <List 
+      inp={inp} 
+      />
     </>
   )
 }
---
+++ src/Components/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function List({inp}) {
+  return (
+    <>  
+        <ul>
+            {inp}
+        </ul>
+    </>
+  )
+}
+
+export default List
