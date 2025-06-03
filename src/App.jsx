c15959d541fd784981cfed8418c28d437ced4199
wayne <waynekim07@gmail.com>
2025-06-03 09:06:25 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,37 @@ import { useState } from 'react'
 import List from './Components/List'
 
 function App() {
-  const [inp,setInp] = useState([])
-  const getItems = e=>setInp(prevInp=>[...prevInp,...e.split(",").map((item,index)=><li key={index}>{item}</li>)])
+  const [inp,setInp] = useState([{id:1,category:"books",item:"Naked man"}])
+  const getItems = (items, category)=>setInp(prevInp=>[...prevInp,...items.split(",").map((item,index)=>{return {id:index+inp.length,item,category}})])
   return (
     <>
-      
+      <select name="" id="category">
+        <option value="books">Books</option>
+        <option value="movies">Movies</option>
+        <option value="music">Music</option>
+      </select>
       <input 
         id='inp'
         type="text"
       />
 
       <button
-        onClick={()=>getItems(document.querySelector("#inp").value)}
+        onClick={()=>getItems(document.querySelector("#inp").value,document.querySelector("#category").value)}
       > Add </button>
-      <List 
-      inp={inp} 
-      />
+      <div className="categories">
+        <List
+        inp={inp}
+        category="Books"
+        />
+        <List
+        inp={inp}
+        category="Movies"
+        />
+        <List
+        inp={inp}
+        category="Music"
+        />
+      </div>
     </>
   )
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function List({inp}) {
+function List({inp,category}) {
+   
+    const list = inp.filter(item=>item.category ===category.toLowerCase()).map((item)=><li key={item.id}>{item.item}</li>)
   return (
     <>  
+        <div className="category-title">{category}</div>
         <ul>
-            {inp}
+            {list}
         </ul>
     </>
   )
